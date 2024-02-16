248c851c82115f04922b1312a09d0c17c07ae39e
hodefiDavid <73063199+hodefiDavid@users.noreply.github.com>
2024-02-16 02:28:54 +0200

    a bit of clean up

--- server.js
+++ server.js
@@ -38,30 +38,31 @@ app.post('/sendOTP', sendOTPController);
 app.post('/resetPassword', resetPasswordController);
 
 
-// ######################################################################################################################
-// Define a route handler sendmail
-app.post('/sendmail', sendMailController);
-
-app.get('/weatherinfo', weatherInfo);
-// Define a route handler for the root path
-app.get('/city', async (req, res) => {
-    let otp = await getCityById(req.query.id);
-    console.log("req.body.id = "+ req.query.id)
-    console.log(otp)
-
-    res.send('your otp code is ' + otp.name);
-});
-
-// Define a route handler for the root path
-app.get('/getotp', async (req, res) => {
-    let otp = await makeOTP();
-    res.send('your otp code is ' + otp);
-});
-
-// ######################################################################################################################
-
-
 // Start the server and listen on the specified port
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+// ######################################################################################################################
+// // Define a route handler sendmail
+// app.post('/sendmail', sendMailController);
+//
+// app.get('/weatherinfo', weatherInfo);
+// // Define a route handler for the root path
+// app.get('/city', async (req, res) => {
+//     let otp = await getCityById(req.query.id);
+//     console.log("req.body.id = "+ req.query.id)
+//     console.log(otp)
+//
+//     res.send('your otp code is ' + otp.name);
+// });
+//
+// // Define a route handler for the root path
+// app.get('/getotp', async (req, res) => {
+//     let otp = await makeOTP();
+//     res.send('your otp code is ' + otp);
+// });
+
+// ######################################################################################################################
+
